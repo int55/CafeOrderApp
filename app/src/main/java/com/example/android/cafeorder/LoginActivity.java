77563931168c9e00a42da70573e0aeafe7ba627a
int55 <iga-pro555@bk.ru>
2020-02-06 15:33:32 +0300

    add files : /CreateOrderActivity.java

--- app/src/main/java/com/example/android/cafeorder/LoginActivity.java
+++ app/src/main/java/com/example/android/cafeorder/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.android.cafeorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        editTextName = (EditText) findViewById(R.id.editNameXML);
+        editTextPass = (EditText) findViewById(R.id.editPassXML);
+    }
+
+    public void onClickCreateOrder(View view) {
+        String name = editTextName.getText().toString().trim(); // .trim() метод убирает лишние пробелы
+        String password = editTextPass.getText().toString().trim();
+            if (!name.isEmpty() && !password.isEmpty()) {
+                Intent intent = new Intent(this, CreateOrderActivity.class);
+                intent.putExtra("name", name);
+                intent.putExtra("password", password);
+                startActivity(intent);
+            }else {
+                Toast.makeText(this, R.string.warning_toast, Toast.LENGTH_SHORT).show();
+            }
     }
 }
